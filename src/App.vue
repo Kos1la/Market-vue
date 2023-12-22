81c78fadf514544d079d1570c52c2880c11291ab
Kos1la <kos1la1998@gmail.com>
2023-12-23 02:46:46 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="bg-slate-700 w-4/5 m-auto h-screen rounded-xl shadow-xl mt-10">
+    <header></header>
+
+    <h1>Market</h1>
+  </div>
+</template>
