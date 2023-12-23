7f5066350fc74f7c57a1d024d4488043233b03d6
Kos1la <kos1la1998@gmail.com>
2023-12-24 02:59:02 +0300

    days 2

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,30 @@
 
 <template>
   <div class="bg-slate-700 w-4/5 m-auto h-screen rounded-xl shadow-xl mt-10">
-    <header></header>
+    <header class="flex justify-between border-b border-slate-600 px-8 py-4">
+      <div>
+        <img src="/header-logo.svg" alt="Logo" class="w-150" />
+        <div>
+          <h2>Diablo Market</h2>
+          <p>Best gamers shop</p>
+        </div>
+      </div>
 
-    <h1>Market commit</h1>
+      <ul>
+        <li>
+          <img src="/basket.svg" alt="basket" />
+          <b>12$ </b>
+        </li>
+        <li>
+          <img src="/Favorite.svg" alt="favorite" />
+          <b>Favorite</b>
+        </li>
+        <li>
+          <img src="/profile.svg" alt="profile" />
+        </li>
+      </ul>
+    </header>
+
+    <h1>Market</h1>
   </div>
 </template>
